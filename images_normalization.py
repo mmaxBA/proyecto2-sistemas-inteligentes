cd42ebb5b2c05cd3e53a74b99cc53008ad3166e7
mmaxBA <max29ba@gmail.com>
2020-05-24 20:16:11 -0500

    database y normalizacion con bounding box y resized

--- images_normalization.py
+++ images_normalization.py
@@ -5,13 +5,43 @@ import cv2
 
 cwd = os.getcwd();
 images_path = os.path.join(cwd, "images")
+save_path = os.path.join(cwd, "images_nor")
 
-th = 128
+THRESHOLD = 200
 
 for dir in os.listdir(images_path):
     image_dir = os.path.join(images_path, dir)
+    save_dir = os.path.join(save_path, dir)
     for filename in os.listdir(image_dir):
-        im = np.array(Image.open(os.path.join(image_dir, filename)))
-        im_bool = im > th
-        im_bin_128 = (im > th) * 255
-        Image.fromarray(np.uint8(im_bin_128)).save(os.path.join(os.path.join(os.path.join(cwd, "images_nor"), dir), filename))
+        img = Image.open(os.path.join(image_dir, filename))
+        if img.mode in ('RGBA', 'LA') or (img.mode == 'P' and 'transparency' in img.info):
+            bg_colour=(255, 255, 255)
+            alpha = img.convert('RGBA').split()[-1]
+            bg = Image.new("RGBA", img.size, bg_colour + (255,))
+            bg.paste(img, mask=alpha)
+            img = bg
+        fn = lambda x : 255 if x > THRESHOLD else 0
+        r = img.convert('L').point(fn, mode='1')
+        image_path = os.path.join(save_dir, filename)
+        r.save(image_path)
+
+        image = cv2.imread(image_path)
+        copy = image.copy()
+        im2 = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        ret,thresh = cv2.threshold(im2,128,255,0)
+        contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+
+        x,y,w,h = 0,0,0,0
+        for cnt in contours:
+            x,y,w,h = cv2.boundingRect(cnt)
+            if h/w > 0.7 and h/w < 1.3:
+                break
+        ROI = image[y:y+h, x:x+w]
+        cv2.imwrite(image_path, ROI)
+        print(filename)
+        cv2.rectangle(copy,(x,y),(x+w,y+h),(36,255,12),2)
+
+        img = Image.open(image_path)
+        resize_img = img.resize((32,32))
+        resize_img.convert("RGB")
+        resize_img.save(image_path)
